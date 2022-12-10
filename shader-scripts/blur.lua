6d802344fb450bddadefa0028c1724d2932af2b5
WarlyFarly-Github! <114765595+WarlyFarly-Github@users.noreply.github.com>
2022-12-10 18:04:23 +0000

    more shaders B) B)

---
+++ shader-scripts/blur.lua
@@ -0,0 +1,55 @@
+local shaderName = "blur"
+function onCreate()
+    shaderCoordFix() -- initialize a fix for textureCoord when resizing game window
+
+    makeLuaSprite("blur")
+    makeGraphic("shaderImage", screenWidth, screenHeight)
+
+   setSpriteShader("shaderImage", "blur")
+
+
+    runHaxeCode([[
+        var shaderName = "]] .. shaderName .. [[";
+        
+        game.initLuaShader(shaderName);
+        
+        var shader0 = game.createRuntimeShader(shaderName);
+        game.camGame.setFilters([new ShaderFilter(shader0)]);
+        game.getLuaObject("blur").shader = shader0; // setting it into temporary sprite so luas can set its shader uniforms/properties
+        game.camHUD.setFilters([new ShaderFilter(game.getLuaObject("blur").shader)]);
+        return;
+    ]])
+end
+
+function onUpdate(elapsed)
+    setShaderFloat("blur", "iTime", os.clock())
+ end
+
+function shaderCoordFix()
+    runHaxeCode([[
+        resetCamCache = function(?spr) {
+            if (spr == null || spr.filters == null) return;
+            spr.__cacheBitmap = null;
+            spr.__cacheBitmapData = null;
+        }
+        
+        fixShaderCoordFix = function(?_) {
+            resetCamCache(game.camGame.flashSprite);
+            resetCamCache(game.camHUD.flashSprite);
+            resetCamCache(game.camOther.flashSprite);
+        }
+    
+        FlxG.signals.gameResized.add(fixShaderCoordFix);
+        fixShaderCoordFix();
+        return;
+    ]])
+    
+    local temp = onDestroy
+    function onDestroy()
+        runHaxeCode([[
+            FlxG.signals.gameResized.remove(fixShaderCoordFix);
+            return;
+        ]])
+        if (temp) then temp() end
+    end
+end
